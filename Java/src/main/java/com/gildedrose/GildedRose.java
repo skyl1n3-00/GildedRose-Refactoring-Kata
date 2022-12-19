d2aa2090b4913ebaba648d0970c53542cfa758ab
mohamed.boufnichel <mohamed.boufnichel>
2022-12-19 15:07:18 +0100

    refactor++

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
 
+    public static final String AGED_BRIE = "Aged Brie";
+    public static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -16,12 +19,12 @@ class GildedRose {
 
     private void updateQualityForItem(Item item) {
         switch (item.name) {
-            case "Aged Brie":
+            case AGED_BRIE:
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
                 }
                 break;
-            case "Backstage passes to a TAFKAL80ETC concert":
+            case PASSES:
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
 
@@ -38,7 +41,7 @@ class GildedRose {
                     }
                 }
                 break;
-            case "Sulfuras, Hand of Ragnaros":
+            case SULFURAS:
                 break;
             default:
                 if (item.quality > 0) {
@@ -46,25 +49,24 @@ class GildedRose {
                 }
                 break;
         }
-
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+        if (!item.name.equals(SULFURAS)) {
             item.sellIn = item.sellIn - 1;
         }
         switch (item.name) {
-            case "Aged Brie":
+            case AGED_BRIE:
                 if (item.sellIn < 0) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
                     }
                 }
                 break;
-            case "Backstage passes to a TAFKAL80ETC concert":
+            case PASSES:
                 if (item.sellIn < 0) {
                     item.quality = 0;
 
                 }
                 break;
-            case "Sulfuras, Hand of Ragnaros":
+            case SULFURAS:
                 // keep it atm
                 break;
             default:
