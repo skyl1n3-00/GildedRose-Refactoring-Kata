5b558b48ad3eea25e5405d6a0f9244e9f60889b6
mohamed.boufnichel <mohamed.boufnichel>
2022-12-19 14:43:17 +0100

    refactor++

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,65 +1,79 @@
 package com.gildedrose;
 
 class GildedRose {
-  Item[] items;
 
-  public GildedRose(Item[] items) {
-    this.items = items;
-  }
+    Item[] items;
 
-  public void updateQuality() {
-    for (Item item : items) updateQualityForItem(item);
-  }
-
-  private void updateQualityForItem(Item item) {
-    if (item.name.equals("Aged Brie")) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-      }
-    } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-      if (item.quality < 50) {
-        item.quality = item.quality + 1;
-
-        if (item.sellIn < 11) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-
-        if (item.sellIn < 6) {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
-          }
-        }
-      }
-    } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-    } else {
-      if (item.quality > 0) {
-        item.quality = item.quality - 1;
-      }
+    public GildedRose(Item[] items) {
+        this.items = items;
     }
 
-    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-      item.sellIn = item.sellIn - 1;
-    }
-    if (item.name.equals("Aged Brie")) {
-      if (item.sellIn < 0) {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
+    public void updateQuality() {
+        for (Item item : items) {
+            updateQualityForItem(item);
         }
-      }
-    } else {
-      if (item.sellIn < 0) {
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-          item.quality = 0;
+    }
 
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-        } else {
-          if (item.quality > 0) {
-            item.quality = item.quality - 1;
-          }
+    private void updateQualityForItem(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                break;
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+                break;
         }
-      }
+
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+        switch (item.name) {
+            case "Aged Brie":
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+                // keep it atm
+                break;
+            default:
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+                break;
+        }
     }
-  }
 }
