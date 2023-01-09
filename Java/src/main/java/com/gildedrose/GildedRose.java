3ef9e3516fef3f68f81b8f9b540f16eefbaa3c70
Yassir Aghraba <yaghraba@sqli.com>
2023-01-09 14:31:42 +0100

    refactoring

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,76 +2,64 @@ package com.gildedrose;
 
 class GildedRose {
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-    public static final int MAX_QUALITY = 50;
-    Item[] items;
+  public static final String AGED_BRIE = "Aged Brie";
+  public static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
+  public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+  public static final int MAX_QUALITY = 50;
+  Item[] items;
 
-    public GildedRose(Item[] items) {
-        this.items = items;
+  public GildedRose(Item[] items) {
+    this.items = items;
+  }
+
+  public void updateQuality() {
+    for (Item item : items) {
+      updateQualityForItem(item);
     }
+  }
 
-    public void updateQuality() {
-        for (Item item : items) {
-            updateQualityForItem(item);
+  private void updateQualityForItem(Item item) {
+    switch (item.name) {
+      case AGED_BRIE:
+        incrementQuality(item, 1);
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          incrementQuality(item, 1);
         }
+        break;
+      case PASSES:
+        if (item.sellIn < 6) {
+          incrementQuality(item, 3);
+        } else if (item.sellIn < 11) {
+          incrementQuality(item, 2);
+        } else {
+          incrementQuality(item, 1);
+        }
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          item.quality = 0;
+        }
+        break;
+      case SULFURAS:
+        // keep it atm
+        break;
+      default:
+        decrementQuality(item);
+        item.sellIn = item.sellIn - 1;
+        if (item.sellIn < 0) {
+          decrementQuality(item);
+        }
+        break;
     }
+  }
 
-    private void updateQualityForItem(Item item) {
-        switch (item.name) {
-            case AGED_BRIE:
-                incrementQuality(item, 1);
-                break;
-            case PASSES:
-                if (item.sellIn < 6) {
-                    incrementQuality(item, 3);
-                } else if (item.sellIn < 11) {
-                    incrementQuality(item, 2);
-                } else {
-                    incrementQuality(item, 1);
-                }
-                break;
-            case SULFURAS:
-                break;
-            default:
-                decrementQuality(item);
-                break;
-        }
-        if (!item.name.equals(SULFURAS)) {
-            item.sellIn = item.sellIn - 1;
-        }
-        switch (item.name) {
-            case AGED_BRIE:
-                if (item.sellIn < 0) {
-                    incrementQuality(item, 1);
-                }
-                break;
-            case PASSES:
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-                break;
-            case SULFURAS:
-                // keep it atm
-                break;
-            default:
-                if (item.sellIn < 0) {
-                    decrementQuality(item);
-                }
-                break;
-        }
+  private void incrementQuality(Item item, int incrementValue) {
+    item.quality = Math.min(item.quality + incrementValue, MAX_QUALITY);
+  }
+
+  private static void decrementQuality(Item item) {
+    if (item.quality > 0) {
+      item.quality = item.quality - 1;
     }
-
-    private void incrementQuality(Item item, int incrementValue) {
-        item.quality = Math.min(item.quality + incrementValue, MAX_QUALITY);
-
-    }
-
-    private static void decrementQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
-        }
-    }
-
+  }
 }
